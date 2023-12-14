e38190a187993e84353c48ce298d83d190ee781a
navir-rahman <navirrahman@gmail.com>
2023-12-14 18:04:58 +0600

    all done

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from static import data
 # Create your views here.
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'data': data})
